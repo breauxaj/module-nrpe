ef28c189825743e80d409cc86dc06686fb818491
Adam Breaux <breauxaj@gmail.com>
2017-04-06 19:30:07 -0400

    we should actually test all these

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -6,7 +6,8 @@ node default {
 
     }
     'RedHat': {
-
+      nrpe::check { 'httpd': command => '/usr/lib64/nagios/plugins/check_procs -w 5:10 -c 5:20 -C httpd' }
+      nrpe::config { 'allowed_hosts' => '172.16.1.1' }
     }
     default: { }
   }
