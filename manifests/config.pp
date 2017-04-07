5a3b4b467c20fd1bd077cce4ed24e44d81d92824
Adam Breaux <abreaux@kbra.com>
2017-04-07 15:24:33 -0400

    go back since we are trying to provide the lens

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,26 +5,12 @@ define nrpe::config (
 
   $key = $title
 
-  case $::operatingsystem {
-    'Amazon': {
-      augeas { "nrpe_cfg/${key}":
-        lens    => 'PHP.lns',
-        incl    => $::nrpe::params::nrpe_config,
-        onlyif  => "get ${key} != '${value}'",
-        changes => "set ${key} '${value}'",
-        notify  => Service[$::nrpe::params::nrpe_service],
-        require => Package[$::nrpe::params::nrpe_package],
-      }
-    }
-    default: {
-      augeas { "nrpe_cfg/${key}":
-        context => $::nrpe::params::nrpe_context,
-        onlyif  => "get ${key} != '${value}'",
-        changes => "set ${key} '${value}'",
-        notify  => Service[$::nrpe::params::nrpe_service],
-        require => Package[$::nrpe::params::nrpe_package],
-      }
-    }
+  augeas { "nrpe_cfg/${key}":
+    context => $::nrpe::params::nrpe_context,
+    onlyif  => "get ${key} != '${value}'",
+    changes => "set ${key} '${value}'",
+    notify  => Service[$::nrpe::params::nrpe_service],
+    require => Package[$::nrpe::params::nrpe_package],
   }
 
 }
