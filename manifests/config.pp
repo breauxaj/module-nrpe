95dd5e6f889f3af659d0178ceaf0c31d306379e1
Adam Breaux <abreaux@kbra.com>
2017-04-07 15:13:50 -0400

    see if we can work around it?

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,12 +5,26 @@ define nrpe::config (
 
   $key = $title
 
-  augeas { "nrpe_cfg/${key}":
-    context => $::nrpe::params::nrpe_context,
-    onlyif  => "get ${key} != '${value}'",
-    changes => "set ${key} '${value}'",
-    notify  => Service[$::nrpe::params::nrpe_service],
-    require => Package[$::nrpe::params::nrpe_package],
+  case $::operatingsystem {
+    'Amazon': {
+      augeas { "nrpe_cfg/${key}":
+        lens    => 'PHP.lns',
+        incl    => $::nrpe::params::nrpe_config,
+        onlyif  => "get ${key} != '${value}'",
+        changes => "set ${key} '${value}'",
+        notify  => Service[$::nrpe::params::nrpe_service],
+        require => Package[$::nrpe::params::nrpe_package],
+      }
+    }
+    default: {
+      augeas { "nrpe_cfg/${key}":
+        context => $::nrpe::params::nrpe_context,
+        onlyif  => "get ${key} != '${value}'",
+        changes => "set ${key} '${value}'",
+        notify  => Service[$::nrpe::params::nrpe_service],
+        require => Package[$::nrpe::params::nrpe_package],
+      }
+    }
   }
 
 }
