073e9a6f295de89f1493724a9ef5ced313ecd199
Adam Breaux <breauxaj@gmail.com>
2017-04-06 20:28:26 -0400

    that works, no errors

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -7,7 +7,7 @@ node default {
     }
     'RedHat': {
       nrpe::check { 'httpd': command => '/usr/lib64/nagios/plugins/check_procs -w 5:10 -c 5:20 -C httpd' }
-      nrpe::config { allowed_hosts: value => '127.0.0.1 172.16.1.1' }
+      nrpe::config { allowed_hosts: value => '127.0.0.1,172.16.1.1' }
     }
     default: { }
   }
